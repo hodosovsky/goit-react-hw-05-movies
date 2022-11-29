52129cb8df79a6a30c742c2f92b696f6bf79de61
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-11-29 21:37:05 +0200

    add styles to movie page

---
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const DescriptionWrap = styled.div`
+  display: flex;
+
+  gap: 10px;
+`;
+
+export { DescriptionWrap };
--- src/pages/MovieDetails/MovieDetailsPage.jsx
+++ src/pages/MovieDetails/MovieDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { MovieByID } from 'services/MovieByID';
 import { useEffect, useState } from 'react';
+import { DescriptionWrap } from './MovieDetails.styled';
 
 export const MovieByIDPage = () => {
   const [movie, setMovie] = useState([]);
@@ -22,17 +23,30 @@ export const MovieByIDPage = () => {
 
   return (
     <section>
-      <h2>{movie.original_title}</h2>
-      <img
-        src={posterURL + movie.poster_path}
-        alt={movie.original_title}
-        width="300"
-      />
-      <br />
-      <a href={movie.homepage} target="blanc">
-        Homepage
-      </a>
-      <p>{movie.overview}</p>
+      <DescriptionWrap className="desription">
+        <div className="poster">
+          <img
+            src={posterURL + movie.poster_path}
+            alt={movie.original_title}
+            width="300"
+          />
+        </div>
+        <div>
+          <h2>{movie.original_title}</h2>
+          {movie.vote_average > 0 && (
+            <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
+          )}
+
+          <br />
+          <a href={movie.homepage} target="blanc">
+            Homepage
+          </a>
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+          {movie.genres && <h4>Genres</h4>}
+          {movie.genres && <p>{movie.genres.map(genr => genr.name + ' ')}</p>}
+        </div>
+      </DescriptionWrap>
       {/* <li key={film.id}>
         <Link className="film-link" to={`/movies/${film.id}`}>
           <img
