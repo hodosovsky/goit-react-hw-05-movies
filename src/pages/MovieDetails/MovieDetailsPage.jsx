77e05432fe6fa50f50cd21f6709d906ad2e7e1c3
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-11-29 20:40:42 +0200

    delete link

--- src/pages/MovieDetails/MovieDetailsPage.jsx
+++ src/pages/MovieDetails/MovieDetailsPage.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { MovieByID } from 'services/MovieByID';
 import { useEffect, useState } from 'react';
 
